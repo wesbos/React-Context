29dc6d0eb5be36bb91d0e1f118f5d6cc1e13cfbe
Wes Bos <wesbos@gmail.com>
2018-03-13 11:13:18 -0400

    hey folks

---
+++ src/App.js
@@ -0,0 +1,66 @@
+import React, { Component } from 'react';
+
+// first we will make a new context
+const MyContext = React.createContext();
+
+// Then create a provider Component
+class MyProvider extends Component {
+  state = {
+    name: 'Wes',
+    age: 100,
+    cool: true
+  }
+  render() {
+    return (
+      <MyContext.Provider value={{
+        state: this.state,
+        growAYearOlder: () => this.setState({
+          age: this.state.age + 1
+        })
+      }}>
+        {this.props.children}
+      </MyContext.Provider>
+    )
+  }
+}
+
+const Family = (props) => (
+  <div className="family">
+    <Person />
+  </div>
+)
+
+class Person extends Component {
+  render() {
+    return (
+      <div className="person">
+        <MyContext.Consumer>
+          {(context) => (
+            <React.Fragment>
+              <p>Age: {context.state.age}</p>
+              <p>Name: {context.state.name}</p>
+              <button onClick={context.growAYearOlder}>🍰🍥🎂</button>
+            </React.Fragment>
+          )}
+        </MyContext.Consumer>
+      </div>
+    )
+  }
+}
+
+
+class App extends Component {
+  render() {
+    return (
+      <MyProvider>
+        <div>
+          <p>I'm the app</p>
+          <Family />
+        </div>
+      </MyProvider>
+    );
+  }
+}
+
+
+export default App;
